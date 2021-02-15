2cdbded3858a7daaa01e7b404821203476f730cb
a7875 <a787574@naver.com>
2021-02-15 10:11:59 +0900

    boardlist

---
+++ src/main/java/imlsw96/spring/mvc/controller/BoardController.java
@@ -0,0 +1,13 @@
+package imlsw96.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("board/list")
+    public String list(){
+        return "board/list";
+    }
+}
