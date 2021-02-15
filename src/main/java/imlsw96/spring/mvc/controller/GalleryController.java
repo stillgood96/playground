701dd18ca5db64e221dbba79d235f3aff6aed793
a7875 <a787574@naver.com>
2021-02-15 10:50:08 +0900

    galleryList

---
+++ src/main/java/imlsw96/spring/mvc/controller/GalleryController.java
@@ -0,0 +1,12 @@
+package imlsw96.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GalleryController {
+    @GetMapping("gallery/list")
+    public String list(){
+        return"gallery/list";
+    }
+}
